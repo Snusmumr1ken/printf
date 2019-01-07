38ceebd4f13d97f65174baa62dc6d32a0b4bc889
Zhivch1k <andrei.nivchik@gmail.com>
2019-01-07 14:53:59 +0200

    added decimal managment

--- manage_decimal.c
+++ manage_decimal.c
@@ -1,29 +1,40 @@
 #include "libp.h"
 
-/*static int			write_prefix(t_format fmt, char *string)
+static int			write_prefix(t_format fmt, char *string)
 {
 	int output_size;
 
 	output_size = 0;
-	if (fmt.sign_plus == 1)
-		output_size += write(1, '+', 1);
+	if (string[0] == '-')
+		output_size += write(1, "-", 1);
+	else if (fmt.sign_plus == 1)
+		output_size += write(1, "+", 1);
 	else if (fmt.sign_space == 1)
-		output_size += write(1, ' ', 1);
+		output_size += write(1, " ", 1);
 	return (output_size);
-}*/
+}
 
 static int			write_value(t_format fmt, char *string)
 {
 	int output_size;
 	int str_len;
+	int i;
 
 	output_size = 0;
-	str_len = ft_strlen(string);
+	str_len = (string[0] == '-') ?
+			(ft_strlen(string) - 1) : (ft_strlen(string));
 	if (str_len == 1 && string[0] == '0' && fmt.precision == 0)
 		return (0);
 	while (str_len++ < fmt.precision)
 		output_size += write(1, "0", 1);
-	output_size += write(1, string, ft_strlen(string));
+	if (string[0] == '-')
+	{
+		i = 1;
+		while (i < ft_strlen(string))
+			output_size += write(1, &string[i++], 1);
+	}
+	else
+		output_size += write(1, string, ft_strlen(string));
 	return (output_size);
 }
 
@@ -32,15 +43,15 @@ static int			write_padding(t_format fmt, char *string)
 	int output_size;
 	int str_len;
 
-	str_len = (ft_strlen(string) < fmt.precision) ?
-			  (fmt.precision) : (ft_strlen(string));
-	str_len = (fmt.sign_plus == 1 || fmt.sign_space == 1) ?
-			  (str_len + 1) : (str_len);
+	str_len = (string[0] == '-') ? (ft_strlen(string) - 1) : ft_strlen(string);
+	str_len = (fmt.precision > str_len) ? fmt.precision : str_len;
 	output_size = 0;
+	if (string[0] == '-' || fmt.sign_plus == 1 || fmt.sign_space == 1)
+		str_len++;
 	while (output_size < fmt.width - str_len)
 	{
-		(fmt.sign_null == 1) ? (write(1, "0", 1)) :
-		(write(1, " ", 1));
+		(fmt.sign_null == 1) ?
+		(write(1, "0", 1)) : (write(1, " ", 1));
 		output_size++;
 	}
 	return (output_size);
@@ -48,29 +59,34 @@ static int			write_padding(t_format fmt, char *string)
 
 int					manage_decimal(t_format fmt, va_list *ap)
 {
-	int 				output_size;
-	unsigned long long	num;
-	char 				*string;
+	int 		output_size;
+	long long	num;
+	char 		*string;
 
 	output_size = 0;
 	if (fmt.len == hh)
-		num = (unsigned char)va_arg(*ap, unsigned);
+		num = (signed char)va_arg(*ap, int);
 	else if (fmt.len == h)
-		num = (unsigned short)va_arg(*ap, unsigned);
+		num = (signed short)va_arg(*ap, int);
 	else if (fmt.len == ll)
-		num = va_arg(*ap, unsigned long long);
+		num = va_arg(*ap, long long);
 	else if (fmt.len == l)
-		num = va_arg(*ap, unsigned long);
+		num = va_arg(*ap, long);
 	else
-		num = va_arg(*ap, unsigned);
-	string = ft_itoa_base_unsigned(num, 10);
+		num = va_arg(*ap, int);
+	string = ft_itoa_base_signed(num, 10);
 	if (fmt.sign_minus == 0)
 	{
+		if (fmt.sign_null == 1)
+			output_size += write_prefix(fmt, string);
 		output_size += write_padding(fmt, string);
+		if (fmt.sign_null == 0)
+			output_size += write_prefix(fmt, string);
 		output_size += write_value(fmt, string);
 	}
 	else
 	{
+		output_size += write_prefix(fmt, string);
 		output_size += write_value(fmt, string);
 		output_size += write_padding(fmt, string);
 	}
