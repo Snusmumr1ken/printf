2d130fd4d7d76b856129035bc4a4ed3d4027313c
Zhivch1k <andrei.nivchik@gmail.com>
2019-01-10 20:28:02 +0200

    float is still do not work

---
+++ ftoa.c
@@ -0,0 +1,63 @@
+#include "libp.h"
+
+static double		pow(double num, double times)
+{
+	int		i;
+	double	n;
+
+	i = -1;
+	n = num;
+	while (++i < times - 1)
+		n *= num;
+	return (n);
+}
+
+static void			reverse(char *str, int len)
+{
+	int i;
+	int j;
+	int temp;
+
+	i = 0;
+	j = len - 1;
+	while (i < j)
+	{
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+		i++;
+		j--;
+	}
+}
+
+static int			intToStr(int x, char str[], int d)
+{
+	int i = 0;
+	while (x)
+	{
+		str[i++] = (x % 10) + '0';
+		x = x / 10;
+	}
+	while (i < d)
+		str[i++] = '0';
+	reverse(str, i);
+	str[i] = '\0';
+	return (i);
+}
+
+void				ftoa(float n, char *res, int afterpoint)
+{
+	int		ipart;
+	float	fpart;
+	int 	i;
+
+	ipart = (int)n;
+	fpart = n - (float)ipart;
+	i = intToStr(ipart, res, 0);
+	if (afterpoint != 0)
+	{
+		res[i] = '.';
+		fpart = fpart * pow(10, afterpoint);
+		intToStr((int)fpart, res + i + 1, afterpoint);
+	}
+}
---
+++ manage_float.c
@@ -0,0 +1,8 @@
+#include "libp.h"
+
+int				manage_float(t_format fmt, va_list *ap)
+{
+	int		output_size;
+
+	return (output_size);
+}
